9f78ec90dfc3630d847ba10b7d84b5b30876f91a
Georgy Litvinov <git@litvinovg.pro>
2019-03-04 21:54:31 +0300

    Added license

--- setSpineHeight.py
+++ setSpineHeight.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8  -*-
+# © 04.03.2019 Georgy Litvinov
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 try:
     import scribus
 except ImportError:
