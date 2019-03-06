86032dccefbf40264ac58d3b75b0a41c000b922b
George Litvinov <git@litvinovg.pro>
2019-03-06 16:39:45 +0300

    Fixes

--- setSpineWidth.py
+++ setSpineWidth.py
@@ -11,7 +11,7 @@ except ImportError:
     print "Unable to import the 'scribus' module. This script will only run within"
     print "the Python interpreter embedded in Scribus. Try Script->Execute Script."
     sys.exit(1)
-newSpineHeight = scribus.valueDialog('Задание высоты корешка','Введите новую высоту корешка')
+newSpineWidth = scribus.valueDialog('Задание ширины корешка','Введите новую ширину корешка')
 
 hasSpineBackground = False
 #set units to mm
@@ -19,10 +19,10 @@ scribus.setUnit(1)
 PageX,PageY = scribus.getPageSize()
 leftX, leftY = scribus.getPosition("left_top_mark")
 rightX, rightY = scribus.getPosition("right_top_mark")
-curSpineHeight = rightX - leftX - 16
-newHeight = float(newSpineHeight)
-spineHeightDiff = newHeight - curSpineHeight
-halfHeightDiff = spineHeightDiff / 2 
+curSpineWidth = rightX - leftX - 16
+newWidth = float(newSpineWidth)
+spineWidthDiff = newWidth - curSpineWidth
+halfWidthDiff = spineWidthDiff / 2 
 
 pageItems = scribus.getPageItems()
 for item in pageItems:
@@ -30,41 +30,41 @@ for item in pageItems:
 		X,Y = scribus.getPosition(item[0])
 		hasSpineBackground = True
 		Xsize,Ysize = scribus.getSize(item[0])
-		scribus.sizeObject(newHeight, Ysize, item[0])
-		scribus.moveObjectAbs(PageX/2 - newHeight/2, Y ,item[0])
+		scribus.sizeObject(newWidth, Ysize, item[0])
+		scribus.moveObjectAbs(PageX/2 - newWidth/2, Y ,item[0])
 	
 for item in pageItems:
 	X,Y = scribus.getPosition(item[0])
 	if item[0].startswith('left_'):
-		scribus.moveObject(-halfHeightDiff, 0 , item[0])
+		scribus.moveObject(-halfWidthDiff, 0 , item[0])
 		if item[0].startswith('left_background'):
 			Xsize,Ysize = scribus.getSize(item[0])
-			leftBackgroundSize = PageX/2 - X + halfHeightDiff + 0.1 
+			leftBackgroundSize = PageX/2 - X + halfWidthDiff + 0.1 
 			if hasSpineBackground:
-				leftBackgroundSize -=  newHeight/2
+				leftBackgroundSize -=  newWidth/2
 			scribus.sizeObject(leftBackgroundSize, Ysize, item[0])
 	if item[0].startswith('right_'):
 		if item[0].startswith('right_background'):
 			Xsize,Ysize = scribus.getSize(item[0])
-			rightBackgroundSize = Xsize + halfHeightDiff
+			rightBackgroundSize = Xsize + halfWidthDiff
 			if hasSpineBackground:
-				rightBackgroundSize -=  halfHeightDiff
+				rightBackgroundSize -=  halfWidthDiff
 			scribus.sizeObject(rightBackgroundSize, Ysize, item[0])
 			rightBackgroundX = PageX/2 -0.1
 # Start after spine ends
 			if hasSpineBackground:
-				rightBackgroundX +=  newHeight/2	
+				rightBackgroundX +=  newWidth/2	
 			scribus.moveObjectAbs(rightBackgroundX, Y ,item[0])
 		else:
-			scribus.moveObject(halfHeightDiff, 0 , item[0])
+			scribus.moveObject(halfWidthDiff, 0 , item[0])
 	if item[0].startswith('spine_logo'):
 		Xsize,Ysize = scribus.getSize(item[0])
-		newBookX = newHeight - newHeight * 0.2
+		newBookX = newWidth - newWidth * 0.2
 		multiplier = newBookX/Xsize
 		newBooKY = Ysize * multiplier
 		scribus.sizeObject(newBookX, newBooKY, item[0])
 		scribus.moveObjectAbs(PageX/2 - newBookX/2 , Y ,item[0])
 
-scribus.setVGuides([(PageX/2), (PageX/2 + newHeight/2), (PageX/2 - newHeight/2)])
+scribus.setVGuides([(PageX/2), (PageX/2 + newWidth/2), (PageX/2 - newWidth/2)])
 
 
